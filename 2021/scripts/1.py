13e18cc74039da1f7a88e69f2cbcb1a5fa04c6ed
Joseph P. Sepich <jps6444@arl.psu.edu>
2021-12-01 08:13:09 -0500

    p2

--- 2021/scripts/1.py
+++ 2021/scripts/1.py
@@ -1,8 +1,10 @@
 import argparse
+from os import curdir
 
 # get input file
 parser = argparse.ArgumentParser(description='Day 1 of advent of code.')
 parser.add_argument('INPUT', type=str, help='Path to input text file.')
+parser.add_argument('--window', action="store_true", default=False, help='Whether to calculate using 3 measurement window')
 
 args = parser.parse_args()
 
@@ -11,8 +13,12 @@ with open(args.INPUT, 'r') as f:
     sonar = [int(x) for x in f.readlines()]
 
 increase_count = 0
-for i in range(len(sonar)-1):
-        if sonar[i] < sonar[i+1]:
+window = 3 if args.window else 1
+for i in range(len(sonar)-window):
+        curr = sonar[i:i+window]
+        next = sonar[i+1:i+1+window]
+        
+        if sum(curr) < sum(next):
             increase_count += 1
 
 print('There are {} instances of increasing depth'.format(increase_count))
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Advent of Code
 
 My solutions to the puzzles in at https://adventofcode.com/
-
-Files are named `day_part.extension`.
